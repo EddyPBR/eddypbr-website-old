e0ea985a2720778dae973607ce96806affa92200
EddyPBR <edvaldojunior1310@gmail.com>
2020-08-13 11:29:25 -0300

    Adding header in home page

--- web/src/pages/Home/index.js
+++ web/src/pages/Home/index.js
@@ -1,10 +1,58 @@
 import React from "react";
+import { FiGithub, FiInstagram, FiLinkedin, FiFacebook } from "react-icons/fi";
 
 import "./styles.css";
 
 const Home = () => {
-  return(
-    <h1>Hello Home</h1>
+  return (
+    <header id="introduction">
+      <div className="container">
+        <img
+          src="https://avatars2.githubusercontent.com/u/48658479?s=400&u=51365e1c6a53cb7c4a0741bb7dda1ebcf64d4417&v=4"
+          alt="Edvaldo Junior"
+        />
+        <h1>Edvaldo Junior</h1>
+        <h2>Desenvolvedor Fullstack</h2>
+        <ul>
+          <li>
+            <a
+              href="https://github.com/EddyPBR"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <FiGithub />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.instagram.com/edvaldo_junior_dev/"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <FiInstagram />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://br.linkedin.com/in/edvaldojuniordev"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <FiLinkedin />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.facebook.com/edvaldo.junior.906638"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <FiFacebook />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </header>
   );
 };
 
