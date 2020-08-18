b983ad15c5c75acbac403e380fb49e5f35954b58
EddyPBR <edvaldojunior1310@gmail.com>
2020-08-18 16:13:57 -0300

    :wrench: Enabling cors in API

--- web/src/pages/Portfolio/index.js
+++ web/src/pages/Portfolio/index.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import Carousel from "../../components/Carousel";
 
 import "./styles.css";
 
+import api from '../../services/api';
+
 const PortfolioPage = () => {
+  useEffect(() => {
+    api
+      .get('portfolios')
+      .then((response) => {
+        // setPortfolios(response.data.projects);
+        console.log(response);
+      });
+  }, ["ok"]);
+
   const portfolios = [
     {
       title: "Lorem ipsum dollor",
