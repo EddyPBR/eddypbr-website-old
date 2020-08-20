cc74d2791c35c3da69d3165678aa59624e462e29
EddyPBR <edvaldojunior1310@gmail.com>
2020-08-20 09:09:36 -0300

    :wrench: Finish About page

--- web/src/pages/About/index.js
+++ web/src/pages/About/index.js
@@ -112,8 +112,58 @@ const About = () => {
             </div>
           </div>
         </div>
-               
-
+        <div className="row">
+          <div>
+            <h1>Formação</h1>
+            <ul className="career-list">
+              <li>
+                <Career
+                  title="Instituto Federal de Ciência e Tecnologia da Paraíba"
+                  details="Graduação, Tecnologia em Telemática"
+                  year="2017-2022"
+                  img_url="https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/Instituto_Federal_Marca_2015.svg/1200px-Instituto_Federal_Marca_2015.svg.png"
+                />
+              </li>
+              <li>
+                <Career
+                  title="Escola Virgem de Lourdes"
+                  details="Técnico, Tecnologia da Informação"
+                  year="2016-2017"
+                  img_url="https://www.conexaomulheresnoescolar.com.br/public/uploads/escolas/200px_1513720787_lourdinas.png"
+                />
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h1>Habilidades</h1>
+            <ul className="career-list">
+              <li>
+                <Career
+                  title="Instituto Federal de Ciência e Tecnologia da Paraíba"
+                  details="Graduação, Tecnologia em Telemática"
+                  year="2017-2022"
+                  tasks={[
+                    "Diretor Executivo - responsavel por líderar todo o time da Árius-Help.",
+                    "Desenvolvedor Fullstack - responsavel por desenvolver sistemas completos, desde a Interface até seu funcionamento lógico interno.",
+                  ]}
+                  img_url="https://avatars1.githubusercontent.com/u/53834932?s=400&v=4"
+                />
+              </li>
+              <li>
+                <Career
+                  title="Desenvolvedor Fullstack & Diretor de Marketing"
+                  details="Jhonny Porto, cantor e compositor"
+                  year="2018-2019"
+                  tasks={[
+                    "Diretor de Marketing - Determinar ações e estrategias de marketing digital.",
+                    "Desenvolvedor Fullstack - Desenvolver um website e atribuir uma lógica de cadastro de agenda de shows para o cantor.",
+                  ]}
+                  img_url="https://studiosol-a.akamaihd.net/tb/652x652/palcomp3-discografia/0/1/1/3/4090f4d34d3d453193bd97bd0a9d57ef.jpg"
+                />
+              </li>
+            </ul>
+          </div>
+        </div>
       </section>
     </main>
   );
